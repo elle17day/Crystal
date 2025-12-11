66d4a609a649e328547432cbfdf485d41d1a0886
RClague2025 <rjclague03@gmail.com>
2025-12-11 18:11:49 +0000

    Updated MapGenerator, TerrNoise
    - Implemented variables
      - octaves
      - persistence
      - lacunarity
     - To add more customizability to the noise map

--- Assets/Scripts/MapGenerator.cs
+++ Assets/Scripts/MapGenerator.cs
@@ -6,16 +6,17 @@ public class MapGenerator : MonoBehaviour
     public int mapHeight;
     public float noiseScale;
 
+    public int octaves;
+    public float persistance;
+    public float lacunarity; 
+
     public bool autoUpdate;
 
     public void GenerateMap()
     {
-        float[,] noiseMap = TerrNoise.GenerateNoiseMap(mapWidth, mapHeight, noiseScale);
+        float[,] noiseMap = TerrNoise.GenerateNoiseMap(mapWidth, mapHeight, noiseScale, octaves, persistance, lacunarity);
 
         MapDisplay display = FindFirstObjectByType<MapDisplay>();
         display.DrawNoiseMap(noiseMap);
-
-
     }
-
 }
