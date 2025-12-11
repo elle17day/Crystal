55b14435dd8202da7abb99fa889d49c3ba0500b6
RClague2025 <rjclague03@gmail.com>
2025-12-11 19:06:40 +0000

    Updated MapGenerator
    - Began work on ability to assign functions to specific height values in noise map
    - Created TextureGenerator

--- Assets/Scripts/MapGenerator.cs
+++ Assets/Scripts/MapGenerator.cs
@@ -2,6 +2,10 @@ using UnityEngine;
 
 public class MapGenerator : MonoBehaviour
 {
+
+    public enum DrawMode {NoiseMap, ColourMap};
+    public DrawMode drawMode;
+
     public int mapWidth;
     public int mapHeight;
     public float noiseScale;
@@ -16,14 +20,45 @@ public class MapGenerator : MonoBehaviour
 
     public bool autoUpdate;
 
+    public TerrainType[] regions;
+
     public void GenerateMap()
     {
         float[,] noiseMap = TerrNoise.GenerateNoiseMap(mapWidth, mapHeight, seed, noiseScale, octaves, persistance, lacunarity, offset);
 
+        Color[] colourMap = new Color[mapWidth * mapHeight];
+        for (int y = 0; y < mapHeight; y++)
+        {
+            for (int x = 0; x < mapWidth; x++)
+            {
+                float currentHeight = noiseMap[x, y];
+
+                for (int i = 0; i < regions.Length; i++)
+                {
+                    if (currentHeight > regions[i].height)
+                    {
+                        colourMap[y * mapWidth + x] = regions[i].colour;
+                        break;
+                    }
+                }
+            }
+        }
+
+        // Generate and display map
         MapDisplay display = FindFirstObjectByType<MapDisplay>();
-        display.DrawNoiseMap(noiseMap);
+        if (drawMode == DrawMode.NoiseMap)
+        {
+            display.DrawNoiseMap(noiseMap);
+        } else if (drawMode == DrawMode.ColourMap)
+        {
+
+        }
+        
+        
     }
 
+
+    // Value Clamps
     void OnValidate()
     {
         if (mapWidth < 1)
@@ -44,4 +79,13 @@ public class MapGenerator : MonoBehaviour
         }
 
     }
+}
+
+// Colour value assignment
+[System.Serializable]
+public struct TerrainType
+{
+    public string name;
+    public float height;
+    public Color colour;
 }
