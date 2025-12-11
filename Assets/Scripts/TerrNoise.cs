5e4ff0e4f7d6bbd4e1f288c4b4613ed325c1758e
RClague2025 <rjclague03@gmail.com>
2025-12-11 15:53:53 +0000

    Update RCBranch
    - Created TerrNoise as pattern generation for procedural terrain generation
    - Created MapGenerator to create terrain

---
+++ Assets/Scripts/TerrNoise.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+using System.Collections;
+
+public static class TerrNoise
+{
+    public static float[,] GenerateNoiseMap(int mapWidth, int mapHeight, float scale)
+    {
+        float[,] noiseMap = new float[mapWidth, mapHeight];
+
+        // Prevents /0 error by camping to 0.0001f
+        if (scale <= 0)
+        {
+            scale = 0.0001f;
+        }
+
+        for (int y = 0; y < mapHeight; y++)
+        {
+            for (int x = 0; x < mapWidth; x++)
+            {
+                float sampleX = x / scale;
+                float sampleY = y / scale;
+
+                float perlinValue = Mathf.PerlinNoise(sampleX, sampleY);
+                noiseMap[x, y] = perlinValue;
+
+
+            }
+        }
+
+        return noiseMap;
+    }
+}
