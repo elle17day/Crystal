a079c496ca32385c07a3396d0098899d57107328
Sagelespice <153180616+Sagelespice@users.noreply.github.com>
2025-12-11 15:08:15 +0000

    added jumping and running

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -6,6 +6,10 @@ public class PlayerMovement : MonoBehaviour
     public float inputX;
     public float inputY;
     public float speed = 20f;
+    public Rigidbody rb;
+    public float jumpAmount = 2;
+    bool isGrounded = true;
+
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -21,5 +25,26 @@ public class PlayerMovement : MonoBehaviour
 
         transform.Translate(Vector3.right * inputX * Time.deltaTime * speed);
         transform.Translate(Vector3.forward * inputY * Time.deltaTime * speed);
+
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            rb.AddForce(Vector3.up * jumpAmount, ForceMode.Impulse);
+            isGrounded = false;
+        }
+
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            speed = 10f;
+        }
+        else
+        {
+            speed = 5f;
+        }
+
+    }
+
+    void OnTriggerEnter()
+    {
+        isGrounded = true;
     }
 }
