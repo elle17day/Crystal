8b58a98db7cfbe7ffefc05e40f1a7c37290d3fa2
RClague2025 <rjclague03@gmail.com>
2025-12-12 16:24:42 +0000

    Update MeshGenerator.cs
    - Implemented new classes to manually calculate normals to allow for future work on removing light seams between chunks

--- Assets/Scripts/MeshGenerator.cs
+++ Assets/Scripts/MeshGenerator.cs
@@ -61,13 +61,48 @@ public class MeshData
         triangleIndex += 3;
     }
 
+    Vector3[] CalculateNormals()
+    {
+        Vector3[] vertexNormals = new Vector3[Vertices.Length];
+        int triengleCount = triangles.Length / 3;
+        for (int i = 0; i < triengleCount; i++)
+        {
+            int normalTriangleIndex = i * 3;
+            int vertexIndexA = triangles[normalTriangleIndex];
+            int vertexIndexB = triangles[normalTriangleIndex + 1];
+            int vertexIndexC = triangles[normalTriangleIndex + 2];
+
+            Vector3 triangleNormal = surfaceNormalFromIndices(vertexIndexA, vertexIndexB, vertexIndexC);
+            vertexNormals[vertexIndexA] += triangleNormal;
+            vertexNormals[vertexIndexB] += triangleNormal;
+            vertexNormals[vertexIndexC] += triangleNormal;
+        }
+
+        for (int i = 0;i < vertexNormals.Length;i++)
+        {
+            vertexNormals[i].Normalize();
+        }
+
+        return vertexNormals;
+    }
+
+    Vector3 surfaceNormalFromIndices(int indexA, int indexB, int indexC) 
+    {
+        Vector3 pointA = Vertices[indexA];
+        Vector3 pointB = Vertices[indexB];
+        Vector3 pointC = Vertices[indexC];
+
+        Vector3 sideAB = pointB - pointA;
+        Vector3 sideAC = pointC - pointA;
+        return Vector3.Cross(sideAB, sideAC).normalized;
+    }
     public Mesh CreateMesh()
     {
         Mesh mesh = new Mesh();
         mesh.vertices = Vertices;
         mesh.triangles = triangles;
         mesh.uv = uvs;
-        mesh.RecalculateNormals();
+        mesh.normals = CalculateNormals();
         return mesh;
     }
 }
