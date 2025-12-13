6f40d97b7f32cb92dad4f4db034a25913bab0fb6
RClague2025 <rjclague03@gmail.com>
2025-12-13 21:07:19 +0000

    Update RCBranch
    - Minor Level Updates

--- Assets/Scripts/FirstPersonCamera.cs
+++ Assets/Scripts/FirstPersonCamera.cs
@@ -5,23 +5,21 @@ public class FirstPersonCamera : MonoBehaviour
 
     public Transform player;
     public float mouseSensitivity = 2f;
+
     float cameraVerticalRotation = 0f;
-
-    bool lockedCursor = true;
-
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        Cursor.visible = false;
         Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
     }
 
     // Update is called once per frame
-    void LateUpdate()
+    void Update()
     {
-        float inputX = Input.GetAxis("Mouse X") * mouseSensitivity;
-        float inputY = Input.GetAxis("Mouse Y") * mouseSensitivity;
+        float inputX = Input.GetAxisRaw("Mouse X") * mouseSensitivity;
+        float inputY = Input.GetAxisRaw("Mouse Y") * mouseSensitivity;
 
         cameraVerticalRotation -= inputY;
         cameraVerticalRotation = Mathf.Clamp(cameraVerticalRotation, -90f, 90f);
