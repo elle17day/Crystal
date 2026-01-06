f2a8a7675488bac4a7269fa53e366c1b55e4a5d6
unknown <elle17day@outlook.com>
2026-01-06 12:16:11 +0000

    Updating enemy functionality for main game purposes

---
+++ Assets/Scripts/MoveToCrystal.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+using UnityEngine.AI;
+
+// Script for moving enemies to crystal
+
+public class MoveToCrystal : MonoBehaviour
+{
+    // Create private variables
+    private NavMeshAgent agent;
+    private GameObject Object;
+
+    private void Awake()
+    {   // Assign variables
+        agent = GetComponent<NavMeshAgent>();
+        Object = this.gameObject;
+    }
+
+    private void Start()
+    {   // Set agents destination as crystal location
+        agent.SetDestination(Crystal.Instance.GetCrystalLocation());
+    }
+
+    void Update()
+    {   // Checks to see if at crystal
+        if (agent.remainingDistance <= 1f)
+        {
+            Debug.Log("Path complete");
+            Crystal.Instance.DamageCrystal(5f);
+            Destroy(Object);
+        }
+    }
+}
