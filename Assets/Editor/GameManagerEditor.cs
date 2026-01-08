81618ce999dddefc00d53cb7a41497eb977879d1
unknown <elle17day@outlook.com>
2026-01-08 14:13:55 +0000

    Find property for game manager editor tool

--- Assets/Editor/GameManagerEditor.cs
+++ Assets/Editor/GameManagerEditor.cs
@@ -35,12 +35,47 @@ public class GameManagerEditor : Editor
     SerializedProperty bossCostProp;
     SerializedProperty bossUnlockWaveProp;
 
+    private void OnEnable()
+    {
+        currentWaveProp = serializedObject.FindProperty("currentWave");
+        waveCostProp = serializedObject.FindProperty("waveCost");
+        waveTimerProp = serializedObject.FindProperty("waveTimer");
+        playerMoneyProp = serializedObject.FindProperty("playerMoney");
+
+        scatterLevelProp = serializedObject.FindProperty("scatterLevel");
+        rapidFireLevelProp = serializedObject.FindProperty("rapidFireLevel");
+        apLevelProp = serializedObject.FindProperty("apLevel");
+        damageLevelProp = serializedObject.FindProperty("damageLevel");
+
+        gruntEnabledProp = serializedObject.FindProperty("gruntEnabled");
+        gruntCostProp = serializedObject.FindProperty("gruntCost");
+        gruntUnlockWaveProp = serializedObject.FindProperty("gruntUnlockWave");
+
+        swarmerEnabledProp = serializedObject.FindProperty("swarmerEnabled");
+        swarmerCostProp = serializedObject.FindProperty("swarmerCost");
+        swarmerUnlockWaveProp = serializedObject.FindProperty("swarmerUnlockWave");
+
+        eliteEnabledProp = serializedObject.FindProperty("eliteEnabled");
+        eliteCostProp = serializedObject.FindProperty("eliteCost");
+        eliteUnlockWaveProp = serializedObject.FindProperty("eliteUnlockWave");
+
+        tankEnabledProp = serializedObject.FindProperty("tankEnabled");
+        tankCostProp = serializedObject.FindProperty("tankCost");
+        tankUnlockWaveProp = serializedObject.FindProperty("tankUnlockWave");
+
+        bossEnabledProp = serializedObject.FindProperty("bossEnabled");
+        bossCostProp = serializedObject.FindProperty("bossCost");
+        bossUnlockWaveProp = serializedObject.FindProperty("bossUnlockWave");
+    }
+
     public override void OnInspectorGUI()
-    {   // Draws inspector
-        DrawDefaultInspector();
+    {
+        serializedObject.Update();
 
         // Reference target script
         GameManager gameManager = (GameManager)target;
+
+        EditorGUILayout.LabelField("Game configuration tool", EditorStyles.boldLabel);
 
 
     }
