e076a47ecb38605221e1256b256c39e081045360
Hui Chen <huic4278@gmail.com>
2026-01-08 11:31:04 +0000

    Update

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -1,20 +1,29 @@
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+<<<<<<< Updated upstream
 using static UnityEngine.Rendering.DebugUI;
 using static UnityEngine.Rendering.DebugUI.Table;
+=======
+using UnityEngine.UI;
+>>>>>>> Stashed changes
 public class UIManager : MonoBehaviour
 {
     public GameObject Pause_Screen;
     public GameObject PlayerUI;
     public GameObject Setting_Screen;
     public GameObject Title_Screen;
+    public GameObject Info_Screen;
+    public GameObject GameOver;
+    public GameObject EndofLevel;
 
+    // Tower Panels
     public GameObject BasicLinePanel;
     public GameObject APPanel;
     public GameObject MultiPanel;
     public GameObject RepeaterPanel;
 
+<<<<<<< Updated upstream
     public GameObject BasicCirclePanel;
     public GameObject IcePanel;
     public GameObject FirePanel;
@@ -26,12 +35,37 @@ public class UIManager : MonoBehaviour
     public void RestartGame()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+=======
+    private bool GamePaused;
+    public GameObject Crystal;
+    private void Awake()
+    {
+        // If there is no instance yet, this is the one
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        else
+        {
+
+            Destroy(gameObject);
+        }
     }
-    public void PlayGame()
+    
+    public void RestartGame()
+    {
+        SceneManager.LoadSceneAsync(SceneManager.GetActiveScene().name); //reset game
+        PlayerUI.SetActive(true);
+        if (Title_Screen != null) Title_Screen.SetActive(false);
+        Cursor.lockState = CursorLockMode.Locked; // Locks curser
+>>>>>>> Stashed changes
+    }
+    public void PlayGame() // close titel screen and show Player UI
     {
         PlayerUI.SetActive(true);
         if (Title_Screen != null) Title_Screen.SetActive(false);
         Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
     }
     public void StopGame()
     {
@@ -41,13 +75,25 @@ public class UIManager : MonoBehaviour
     {
         Setting_Screen.SetActive(true);
     }
-    public void Back()
+
+    public void InfoScreen()
+    {
+        Info_Screen.SetActive(true);
+    }
+    public void Back() //Close current panel
     {
         Setting_Screen.SetActive(false);
+        Info_Screen.SetActive(false);
     }
     private void Update()
     {
+<<<<<<< Updated upstream
         if (PlayerUI != true)
+=======
+        //HealthC = Crystal.GetComponent<Crystal>().
+
+        if (Input.GetKeyDown(KeyCode.Escape)) // Press escap for pause menu
+>>>>>>> Stashed changes
         {
             if (Input.GetKeyDown(KeyCode.Escape))
             {
@@ -55,7 +101,7 @@ public class UIManager : MonoBehaviour
                 else Pause();
             }
         }
-        if (Input.GetKeyDown(KeyCode.Q))
+        if (Input.GetKeyDown(KeyCode.Q)) // show tower panels
         {
             if (BasicLinePanel != null) BasicLinePanel.SetActive(true);
         }
@@ -63,12 +109,10 @@ public class UIManager : MonoBehaviour
     public void Pause()
     {
         if (Pause_Screen != null) Pause_Screen.SetActive(true);
-
-        Time.timeScale = 0f;
+        Time.timeScale = 0f; //stop all movement
         GamePaused = true;
 
-        // Optional: Lock cursor for menus
-        Cursor.lockState = CursorLockMode.None;
+        Cursor.lockState = CursorLockMode.None; // Unlock curser in pause menu
         Cursor.visible = true;
     }
     public void Resume()
@@ -78,12 +122,11 @@ public class UIManager : MonoBehaviour
         Time.timeScale = 1f; // Resume game time
         GamePaused = false;
 
-        // Optional: Hide cursor for gameplay
         Cursor.lockState = CursorLockMode.Locked;
-        Cursor.visible = false;
+        Cursor.visible = false; //Hide cursor for gameplay
     }
 
-    public void CloseButton()
+    public void CloseButton() //Close Tower panels
     {
         Cursor.lockState = CursorLockMode.None;
         //if (BasicCirclePanel != null) 
@@ -99,28 +142,27 @@ public class UIManager : MonoBehaviour
         if (RepeaterPanel != null) RepeaterPanel.SetActive(false);
 
     }
-    public void MainMenu()
+    public void MainMenu() // Return to main menu and close other panels
     {
         Title_Screen.SetActive(true);
         if (Pause_Screen != null) Pause_Screen.SetActive(false);
         if (PlayerUI != null) PlayerUI.SetActive(false);
+        if (GameOver != null) GameOver.SetActive(false);
+        if (EndofLevel != null) EndofLevel.SetActive(false);
     }
     public void APPanuel()
     {
         APPanel.SetActive(true);
-        if (BasicLinePanel != null) BasicLinePanel.SetActive(false);
     }
 
     public void MultiPanuel()
     {
         MultiPanel.SetActive(true);
-        if (BasicLinePanel != null) BasicLinePanel.SetActive(false);
     }
 
     public void RepeaterPanuel()
     {
         RepeaterPanel.SetActive(true);
-        if (BasicLinePanel != null) BasicLinePanel.SetActive(false);
     }
     public void IcePanuel()
     {
